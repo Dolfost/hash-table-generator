80239b9197d8f2664fa70339b040bc5f6b4fea44
Vladyslav Rehan <rehanvladyslav@gmail.com>
2023-11-16 16:57:00 +0200

    Move over to QtCreator

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <ctime>
+#include "mainwindow.h"
 
-#include "chainHashTable.hpp"
-#include "doublyLinkedList.hpp"
+#include <QApplication>
 
-int hash(int value, int max) {
-	return value % (max);
-}
-
-int main(void) {
-	ChainHashTable<int>* table = new ChainHashTable<int>(8, hash, 2);
-
-	for (int i = 0; i < 100; i++)
-		table->insert(rand() % 30);
-	
-	std::cout << "count: " << table->count << std::endl;
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
